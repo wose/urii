a4020efda42ac91c186e0d507757ab0faef4b5c8
Sebastian Woetzel <wose@zuendmasse.de>
2017-03-17 20:56:44 +0100

    rustfmt

--- src/info.rs
+++ src/info.rs
@@ -30,22 +30,22 @@ impl InfoPlugin {
 impl MsgHandler for InfoPlugin {
     fn on_priv_msg(&mut self, irc: IrcServer, _message: &Message, target: &str, msg: &str) {
         if let Some(cap) = self.info_re.captures(&msg) {
-            self.store.set(module_path!(), cap[1].trim(),
-                           Info {
-                               thing: cap[1].trim().into(),
-                               description: cap[0].trim().into()
-                           })
+            self.store
+                .set(module_path!(),
+                     cap[1].trim(),
+                     Info {
+                         thing: cap[1].trim().into(),
+                         description: cap[0].trim().into(),
+                     })
                 .unwrap();
         }
 
         if let Some(cap) = self.query_re.captures(&msg) {
             match self.store.get::<Info>(module_path!(), &cap[1].trim()) {
                 Ok(info) => {
-                    irc.send_privmsg(&target,
-                                     format!("{}", info.description).as_str())
-                        .unwrap();
-                },
-                Err(_) => ()
+                    irc.send_privmsg(&target, format!("{}", info.description).as_str()).unwrap();
+                }
+                Err(_) => (),
             }
         }
     }
